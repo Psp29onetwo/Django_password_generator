dffb37f4d150dde6633aa6b3fba9eab94c9ff8c7
Paurav(#PSP) <pspofficial2912@gmail.com>
2020-06-01 20:21:46 +0530

    Forms redirection added

--- password_generator/generator/views.py
+++ password_generator/generator/views.py
@@ -1,8 +1,11 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+
+
 # Create your views here.
 def home(request):
-    return render(request, 'generator/home.html') # third arg for whatever we want to pass with request to the html file
+    return render(request, 'generator/home.html')
+
 
 def password(request):
     return render(request, 'generator/password.html')
