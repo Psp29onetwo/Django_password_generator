80015b15ffccc2a12ea85b20fce4734b181ea4e1
Paurav(#PSP) <pspofficial2912@gmail.com>
2020-06-01 19:36:39 +0530

    Project created

---
+++ password_generator/generator/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+# Create your views here.
+def home(request):
+    return HttpResponse('Welcome Home!')
+
+def psp(request):
+    return HttpResponse('Hi there my name is #PSP')
