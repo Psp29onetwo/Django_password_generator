8456f1ea2fc61082fe1b1858d3809729e1af15d3
Paurav(#PSP) <pspofficial2912@gmail.com>
2020-06-02 15:48:19 +0530

    Forms redirection added

--- password_generator/generator/views.py
+++ password_generator/generator/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+import random
 
 
 # Create your views here.
@@ -8,4 +9,26 @@ def home(request):
 
 
 def password(request):
-    return render(request, 'generator/password.html')
+
+    characters = list('abcdefghijklmnopqrstuvwxyz')
+
+
+    if request.GET.get('uppercase'):
+        characters.extend(list('ABCDEFGHIJKLMNOPQRSTUVWXYZ'))
+
+    if request.GET.get('special'):
+        characters.extend(list('!@#$%^&*()'))
+
+    if request.GET.get('numbers'):
+        characters.extend(list('1234567890'))
+
+
+    length = int(request.GET.get('length', 12))
+    thepassword = ''
+
+    for c in range(length):
+        thepassword += random.choice(characters)
+    return render(request, 'generator/password.html',{'password' : thepassword} )
+
+def about(request):
+    return render(request, 'generator/about.html')
